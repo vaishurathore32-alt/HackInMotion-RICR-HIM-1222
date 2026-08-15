b90a4d21414df868cd390cbde09e31de2fba0066
solankiesh296-gif <solankiesh296@gmail.com>
2026-08-15 11:08:21 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -245,7 +245,7 @@ app.get("/", (req, res) => {
 // START SERVER
 // ===============================
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
